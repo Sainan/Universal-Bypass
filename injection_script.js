ee0f45509ed65310b27fd4670e893faf9d6e84e8
Лья <71004056+Lya-chan@users.noreply.github.com>
2020-09-09 15:21:52 +0700

    JRE8

--- injection_script.js
+++ injection_script.js
@@ -1686,6 +1686,27 @@ ensureDomLoaded(()=>{
 		}
 		a.href+=a.textContent.replace(/[^\w]/g,"").toLowerCase()
 	}))
+	ddomainBypass("oracle.com", () => {
+		ensureDomLoaded(() => {
+		    // get All Download Links
+		    let jdk_links = document.querySelectorAll("[data-file]");
+			jdk_links.forEach((element) => {
+			    // https://gist.github.com/wavezhang/ba8425f24a968ec9b2a8619d7c2d86a6#gistcomment-3377085
+			    var link = element.getAttribute("data-file");
+			    console.log(link);
+			    jre8 = RegExp("download.oracle.com/otn/java/jdk/8u([0-9]*)-b([0-9]*)/([a-z0-9]{32})/(.*)$", "g").exec(link);
+			    if (jre8&&jre8[3]) {
+				os_type = RegExp("8u[0-9]*-([^-]*)-").exec(jre8[4])[1];
+				// MacOs 
+				os_type = (os_type == "macosx") ? "unix" : os_type;
+				// assign DDL link
+				element.href = "https://javadl.oracle.com/webapps/download/GetFile/1.8.0_"+jre8[1]+"-b"+jre8[2]+"/"+jre8[3]+"/"+os_type+"-i586/"+jre8[4];
+			    }
+
+			});
+
+		})
+	})
 	if(document.querySelector(".sorasubmit"))
 	{
 		document.querySelector(".sorasubmit").click()
