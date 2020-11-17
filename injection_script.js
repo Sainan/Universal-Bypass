7a51c3a4609f1095825a246c1bd0bfcfe2beddf7
cloud <62519659+respecting@users.noreply.github.com>
2020-11-17 08:27:54 -0700

    Add akwams.com bypass (close #1636)

--- injection_script.js
+++ injection_script.js
@@ -732,6 +732,7 @@ domainBypass("linksunlocked.com",()=>{
 	}
 })
 domainBypass("samehadaku.vip",()=>ifElement("div.close-button",a=>a.click()))
+domainBypass("akwams.com",()=>ifElement('.download-link',a=>safelyNavigate(a.href)))
 //Insertion point for bypasses running before the DOM is loaded.
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile)\.com|(punchsubs|zedge|fex)\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|(uploadfree|freeupload)\.info|fstore\.biz))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/thesimsresource\.com\/downloads\/details\/id\//,()=>window.setTimeout=f=>setTimeout(f,1))
